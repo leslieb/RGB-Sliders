c940d2ade3641747828af1ee32651e5157bf414f
Leslie Borrell <leslie@sols.com>
2016-05-04 12:53:04 -0400

    created sliders, navigation controller and overlay

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,7 +28,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
-// learning about version control
